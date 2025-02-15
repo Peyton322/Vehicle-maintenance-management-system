ad7da8a18374210c7bc4c40775b6b27ec3c177dd
MakerRabbitTech <makerrabbittech@gmail.com>
2025-02-15 14:15:42 +0800

    部屬

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';  // 改為 HashRouter
 import App from './App';
 import Query from './Query' 
 import CarStatus from './CarStatus';
@@ -10,13 +10,13 @@ import CustomerHistory from './CustomerHistory';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>  
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/query" element={<Query />} />
         <Route path="/customer-history/:id" element={<CustomerHistory />} />
         <Route path="/car-status/:plateNumber" element={<CarStatus />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>  
   </React.StrictMode>
 );
